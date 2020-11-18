42a9506c07fcf954451533c1555040090b86e733
Andrea Colleoni <andrea@colleoni.info>
2020-11-18 17:12:39 +0100

    esercizi

--- modulo-04/eclipse/mod-04-lezione-01/src/lezione01/ControlloDiFlusso.java
+++ modulo-04/eclipse/mod-04-lezione-01/src/lezione01/ControlloDiFlusso.java
@@ -3,7 +3,7 @@ package lezione01;
 public class ControlloDiFlusso {
 
 	public static void main(String[] args) {
-		// branching con if
+		// branching (ramificazione) con if
 		int a = 10;
 		System.out.println("inizio del programma");
 		if (a > 20) {
@@ -19,6 +19,40 @@ public class ControlloDiFlusso {
 		} else {
 			// else è opzionale; viene eseguita se TUTTE le condizioni precedenti sono false
 			System.out.println("a è compreso tra 5 e 20");
+		}
+		System.out.println("-----");
+		// looping (ciclo) con for
+		// for (istr 1; istr 2; istr 3)
+		// istr 1: viene eseguita solo UNA volta prima di entrare
+		//         nel ciclo
+		// istr 2: (boolean) viene verificata prima di ogni iterazione
+		//         è la condizione di permanenza nel ciclo
+		// istr 3: viene eseguita ad ogni fine ciclo
+		for (int i = 0; i < 10; i += 2) {
+			System.out.println("istruzione n. 1 ripetuta => iterazione n." + i);
+		}
+		// System.out.println("valore di i alla fine del ciclo: " + i); // NO!!
+		System.out.println("-----");
+		// casi limite
+		int j = 0;
+		for (; j < 10; j += 2) {
+			System.out.println("istruzione n. 2 ripetuta => iterazione n." + j);
+		}
+		System.out.println("valore di j alla fine del ciclo: " + j);
+		System.out.println("-----");
+		int k = 0;
+		for (; k < 10;) {
+			System.out.println("istruzione n. 3 ripetuta");
+			k += 2;
+		}
+		System.out.println("-----");
+		int h = 0;
+		for(;;) {
+			if (h >= 10) {
+				break; // istruzione che mi permette di uscire da un loop
+			}
+			System.out.println("istruzione n. 4 ripetuta");
+			h += 2;
 		}
 		System.out.println("fine del programma");
 	}

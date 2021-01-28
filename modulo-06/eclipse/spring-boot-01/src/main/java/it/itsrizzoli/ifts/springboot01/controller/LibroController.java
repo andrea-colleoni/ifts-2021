dce1c48d0fe95f1b549d0d8e8d9a24480cdc047c
Andrea Colleoni <andrea@colleoni.info>
2021-01-28 15:55:44 +0100

    delete

--- modulo-06/eclipse/spring-boot-01/src/main/java/it/itsrizzoli/ifts/springboot01/controller/LibroController.java
+++ modulo-06/eclipse/spring-boot-01/src/main/java/it/itsrizzoli/ifts/springboot01/controller/LibroController.java
@@ -4,6 +4,7 @@ package it.itsrizzoli.ifts.springboot01.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,11 @@ public class LibroController {
 					repository.save(libro);
 				});
 		return repository.findById(codiceISBN).get();
+	}
+	
+	@DeleteMapping("/libri/{codiceISBN}")
+	public void eimina(@PathVariable String codiceISBN) {
+		repository.deleteById(codiceISBN);
 	}
 
 }
